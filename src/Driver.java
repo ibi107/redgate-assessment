65d9d99a3553fb879472473640f45acda23d6da1
Ibtesam Ahmad <zlac459@live.rhul.ac.uk>
2023-12-16 06:57:38 +0000

    Wrote missing Javadoc for Driver class.

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,8 @@
+/**
+ * The driver class which runs the main method to initialise a DataMasker.
+ *
+ * @author Ibtesam Ahmad
+ */
 public class Driver {
   public static void main(String[] args) {
     DataMasker dataMasker = new DataMasker(args[0], args[1]);
